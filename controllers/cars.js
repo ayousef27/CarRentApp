b6cb3dca2e0c8356d3e73df142c5cbf2273bcc32
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-12-08 19:52:15 +0300

    back with original

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,15 +3,5 @@ const { application } = require('express')
 //import model
 const User = require('../models/user')
 const Car = require('../models/car')
-router.get('/', async (req, res) => {
-  const currentUser = await User.findById(req.session.user._id)
-  res.render('cars/index.ejs', {
-    cars: currentUser.cars
-  })
-})
 
-//async because we will do database operation here
-router.get('/new', async (req, res) => {
-  res.render('cars/new.ejs')
-})
 module.exports = router
