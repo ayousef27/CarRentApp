277680f93761581c8325f0607342cb756020db44
ayousef27 <ahmenero27@gmail.com>
2024-12-10 11:07:27 +0300

    user

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: Number,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
